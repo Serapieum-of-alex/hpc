e223f24207fff804fb48cf08956b0940eeb0cf83
Mostafa Farrag <moah.farag@gmail.com>
2023-03-30 21:34:15 +0200

    update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,6 @@
-from typing import List
 import numpy as np
 import pytest
 
-
-# @pytest.fixture(scope="module")
-# def arr() -> np.ndarray:
-#     return np.load("examples/data/arr.npy")
+@pytest.fixture(scope="module")
+def arr() -> np.ndarray:
+    return np.load("tests/data/arr.npy")
