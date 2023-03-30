7b05083ae29d899819c077686c4e821121caeda6
Mostafa Farrag <moah.farag@gmail.com>
2023-03-30 19:53:41 +0200

    initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+from typing import List
+import numpy as np
+import pytest
+
+
+# @pytest.fixture(scope="module")
+# def arr() -> np.ndarray:
+#     return np.load("examples/data/arr.npy")
---
+++ tests/test_plot_array.py
@@ -0,0 +1,6 @@
+from typing import List
+import numpy as np
+
+
+def test_import():
+    import numpy
