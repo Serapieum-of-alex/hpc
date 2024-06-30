ea719afff48b5b662cbc4659a7e66b91f58f6222
Mostafa Farrag <moah.farag@gmail.com>
2024-06-30 19:44:44 +0200

    dependencies (#9)
    * update dependencies
    
    * update
    
    * update
    
    * correct schipy version
    
    * update pypi workflow
    
    * downgrade pip to 24.0
    
    * downgrade pip to 24.0
    
    * add setuptools to dependencies
    
    * remove python from the environment.yml and add conda workflow
    
    * latest pip in conda-forge is 24.0
    
    * rename job
    
    * add .run to ignore

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements_dev = [line.strip() for line in open("requirements-dev.txt").readli
 
 setup(
     name="hpc-utils",
-    version="0.1.3",
+    version="0.1.4",
     description="numpy utility package",
     author="Mostafa Farrag",
     author_email="moah.farag@gmail.come",
